32cb4758f090199d73fc516cd8327f5c181bb843
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-08 18:34:25 -0700

    Update Attributes context

--- test/potluck_spec.rb
+++ test/potluck_spec.rb
@@ -9,5 +9,13 @@ describe Potluck do
     it 'exists' do
       expect(potluck).to be_a(Potluck)
     end
+
+    it 'has a date' do
+      expect(potluck.date).to eq("7-13-18")
+    end
+
+    it 'has dishes' do
+      expect(potluck.dishes).to eq([])
+    end
   end
 end
