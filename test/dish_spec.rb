981ff2f38f65350b6200bc1b3ed82390b112ddd3
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-08 17:01:18 -0700

    Add type to spec

--- test/dish_spec.rb
+++ test/dish_spec.rb
@@ -8,5 +8,9 @@ describe Dish do
     it 'has a name' do
       expect(dish.name).to eq("Couscous salad")
     end
+
+    it 'has a type' do
+      expect(dish.type).to eq(:appetizer)
+    end
   end
 end
