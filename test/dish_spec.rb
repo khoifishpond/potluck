64194d801157dcc243b6dc47e0258e572965c2e0
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-08 18:11:23 -0700

    Update spec

--- test/dish_spec.rb
+++ test/dish_spec.rb
@@ -4,6 +4,10 @@ require './lib/dish'
 describe Dish do
   context 'Attributes' do
     dish = Dish.new("Couscous salad", :appetizer)
+
+    it 'exists' do
+      expect(dish).to be_a(Dish)
+    end
     
     it 'has a name' do
       expect(dish.name).to eq("Couscous salad")
